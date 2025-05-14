3f58199f632d754db44df688e62ea8cf61c92963
avilazdudu <avilazdudu@gmail.com>
2025-05-13 22:27:23 -0300

    Finalizado Amigo Secreto

---
+++ amigo-secreto/js/app.js
@@ -0,0 +1,40 @@
+let amigos = [];
+
+function adicionar(){
+    let amigo = document.getElementById("nome-amigo");
+    let lista = document.getElementById("lista-amigos");
+    amigos.push(amigo.value);
+    if (lista.textContent == "") {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent += ", " + amigo.value;
+    }
+    amigo.value = "";
+}
+
+function sortear(){
+ embaralha(amigos)
+ let sorteio = document.getElementById("lista-sorteio");
+
+ for (let i = 0; i < amigos.length; i++) {
+    sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '---> ' + amigos[i + 1] + '<br>';
+ }
+ console.log(lista);
+}
+
+function embaralha(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar(){
+    let amigo = document.getElementById("nome-amigo");
+    let lista = document.getElementById("lista-amigos");
+    let sorteio = document.getElementById("lista-sorteio");
+    amigos = [];
+    amigo.value = "";
+    lista.textContent = "";
+    sorteio.innerHTML = "";
+}
